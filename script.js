7a2c84d80f0e319bef20de7060ec7ea75bdf9dc4
Nuno Sousa <2807711+nunosousa-dev@users.noreply.github.com>
2026-02-13 12:50:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gifStages = [
     "https://media.tenor.com/36d8ovZxZsgAAAAj/bubu-bubu-dudu.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
+    "https://media.tenor.com/21Rpgv7I-jMAAAAi/bubu-dudu-sseeyall.gif",  // 1 confused
+    "https://media.tenor.com/EKrsFntSuSgAAAAi/sad.gif",             // 2 pleading
     "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
     "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
     "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
